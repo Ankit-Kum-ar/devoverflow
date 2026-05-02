3453bde9a465fe4f58100f1f2721160de4789029
Ankit-0009 <ankitcode2511@gmail.com>
2026-05-03 00:24:31 +0530

    Implement the server action handler

---
+++ lib/handlers/action.ts
@@ -0,0 +1,49 @@
+'use server';
+
+import { ZodError, ZodSchema } from "zod";
+import { UnauthorizedError, ValidationError } from "../http-errors";
+import { Session } from "next-auth";
+import { auth } from "@/auth";
+import dbConnect from "../mongoose";
+
+// This is server action handler, it will be called by client components when they want to perform some server-side logic.
+type ActionOptions<T> = {
+    params?: T;
+    schema?: ZodSchema<T>;
+    authorize?: boolean;
+}
+
+async function action<T>({
+    params,
+    schema,
+    authorize = false
+}: ActionOptions<T>) {
+    if (schema && params) {
+        try {
+            schema.parse(params);
+        } catch (error) {
+            if(error instanceof ZodError) {
+                return new ValidationError(error.flatten().fieldErrors as Record<string, string[]>);
+            } else {
+                return new Error("Schema Validation Error...");
+            }
+        }
+    }
+
+    let session: Session | null = null;
+    if (authorize) {
+        session = await auth();
+        if (!session) {
+            return new UnauthorizedError();
+        }
+    }
+
+    await dbConnect();
+
+    return {
+        session,
+        params
+    };
+}
+
+export default action;
