b2cde525941dcf94cab17a8f496da5a58cf9cb99
Ankit-0009 <ankitcode2511@gmail.com>
2026-05-07 09:26:19 +0530

    SignIn Implementation by server-actions

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -2,13 +2,14 @@
 
 import { ActionResponse, ErrorResponse } from "@/types/global";
 import action from "../handlers/action";
-import { SignUpSchema } from "../validations";
+import { SignInSchema, SignUpSchema } from "../validations";
 import handleError from "../handlers/error";
 import mongoose from "mongoose";
 import User from "@/database/user.model";
 import bcrypt from "bcryptjs";
 import Account from "@/database/account.model";
 import { signIn } from "@/auth";
+import { NotFoundError } from "../http-errors";
 
 export async function signUpWithCredentials(
   params: AuthCredentials
@@ -92,5 +93,65 @@ export async function signUpWithCredentials(
     return handleError(error) as ErrorResponse; // We return the error to the client.
   } finally {
     await session.endSession(); // Finally, we end the session.
+  }
+}
+
+export async function signInWithCredentials(
+  params: Pick<AuthCredentials, "email" | "password">
+): Promise<ActionResponse> {
+  // 1. Validate the data of params.
+  const validationResult = await action({
+    params,
+    schema: SignInSchema,
+  });
+
+  // 2. If the validation fails, we return the validation error to the client.
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  // 3. Extract the validated params from the validation result.
+  const { email, password } = validationResult.params!;
+
+  try {
+    // 5. Start checks user already exists by email or not.
+    const exisitingUser = await User.findOne({ email });
+    if (!exisitingUser) {
+      throw new NotFoundError("User");
+    }
+
+    // 6. If the user exists, we need to check if the password is correct or not, we can do this by comparing the hashed password stored in the database with the password provided by the user using bcrypt.
+    const existingAccount = await Account.findOne({
+      provider: "credentials",
+      providerAccountId: email,
+    });
+
+    if (!existingAccount) {
+      throw new NotFoundError("Account");
+    }
+
+    // 7. If the account exists, we compare the hashed password with the provided password.
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingAccount.password
+    );
+    if (!isPasswordValid) {
+      throw new Error("Invalid password");
+    }
+
+    // 10. Call signIn from auth.ts to create a session for the user, this will allow the user to be signed in immediately after signing up.
+    await signIn("credentials", {
+      email,
+      password,
+      redirect: false, // We set redirect to false because we don't want to redirect the user after signing in, we just want to create a session for the user.
+    });
+
+
+    // 12. Finally, we return a success response to the client.
+    return {
+      success: true,
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse; // We return the error to the client.
   }
 }
