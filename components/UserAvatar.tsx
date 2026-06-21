545a89d13c5bd738db64920a9da13337a13428ef
Ankit-0009 <ankitcode2511@gmail.com>
2026-06-21 18:43:27 +0530

    Implement the Question Details UI and its server action. We have use two new packages that are next-mdx-remote & bright for the UI part.

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -2,15 +2,17 @@ import ROUTES from "@/app/constants/route";
 import Link from "next/link";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 interface Props {
   id: string;
   name: string;
   image?: string;
   className?: string;
+  fallbackClassName?: string;
 }
 
-const UserAvatar = ({ id, name, image, className = "h-9 w-9" }: Props) => {
+const UserAvatar = ({ id, name, image, className = "h-9 w-9", fallbackClassName }: Props) => {
   const initials = name
     .split(" ")
     .map((word: string) => word[0])
@@ -29,7 +31,7 @@ const UserAvatar = ({ id, name, image, className = "h-9 w-9" }: Props) => {
             height={36}
           />
         ) : (
-          <AvatarFallback className="primary-vairant font-space-grotesk font-bold tracking-wider text-white">
+          <AvatarFallback className={cn("primary-vairant font-space-grotesk font-bold tracking-wider text-white", fallbackClassName)}>
             {initials}
           </AvatarFallback>
         )}
