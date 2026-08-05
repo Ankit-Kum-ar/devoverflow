cbb06daff3b800710c6b8d612e6f4bdcb8eb84ad
Ankit-0009 <ankitcode2511@gmail.com>
2026-08-05 23:18:26 +0530

    Implement the votes UI

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -12,6 +12,7 @@ import { RouteParams, Tag } from "@/types/global";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { after } from "next/server";
+import Votes from "../../../../components/votes/Votes";
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -55,7 +56,7 @@ const QuestionDetails = async ({ params }: RouteParams) => {
           </div>
 
           <div className="flex justify-end">
-            <p>Votes</p>
+            <Votes upvotes={question.upvotes} hasupVoted={true} downvotes={question.downvotes} hasdownVoted={false} />
           </div>
         </div>
 
---
+++ components/votes/Votes.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { formatNumber } from "@/lib/utils";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import React, { useState } from "react";
+import { toast } from "sonner";
+
+interface Props {
+  upvotes: number;
+  downvotes: number;
+  hasupVoted: boolean;
+  hasdownVoted: boolean;
+}
+
+const Votes = ({ upvotes, downvotes, hasupVoted, hasdownVoted }: Props) => {
+  const session = useSession();
+  const userId = session.data?.user?.id;
+  const [isLoading, setIsLoading] = useState(false);
+  const handleVote = async (voteType: "upvote" | "downvote") => {
+    if (!userId) return toast.error("You must be logged in to vote.");
+    setIsLoading(true);
+    try {
+      const successMessage =
+        voteType === "upvote"
+          ? `Upvote ${!hasupVoted ? "added" : "removed"} successfully!`
+          : `Downvote ${!hasdownVoted ? "added" : "removed"} successfully!`;
+      toast.success(successMessage);
+    } catch (error) {
+      toast.error("An error occurred while voting. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="flex-center gap-2.5">
+      <div className="flex-center gap-1.5">
+        <Image
+          src={hasupVoted ? "/icons/upvoted.svg" : "/icons/upvote.svg"}
+          width={18}
+          height={18}
+          alt="Upvote Icon"
+          className={`cursor-pointer ${isLoading && "opacity-50"}`}
+          arial-label="Upvote Icon"
+          onClick={() => !isLoading && handleVote("upvote")}
+        />
+
+        <div className="flex-center background-light700_dark400 min-w-5 rounded-sm p-1">
+          <p className="subtle-medium text-dark400_light900">
+            {formatNumber(upvotes)}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex-center gap-1.5">
+        <Image
+          src={hasdownVoted ? "/icons/downvoted.svg" : "/icons/downvote.svg"}
+          width={18}
+          height={18}
+          alt="Upvote Icon"
+          className={`cursor-pointer ${isLoading && "opacity-50"}`}
+          arial-label="Upvote Icon"
+          onClick={() => !isLoading && handleVote("downvote")}
+        />
+
+        <div className="flex-center background-light700_dark400 min-w-5 rounded-sm p-1">
+          <p className="subtle-medium text-dark400_light900">
+            {formatNumber(downvotes)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Votes;
