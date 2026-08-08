3fd494e738761573216db21f1bee97add1091ec7
Ankit-0009 <ankitcode2511@gmail.com>
2026-08-08 23:06:57 +0530

    toggleSaveQuestion server action for collection

--- app/constants/route.ts
+++ app/constants/route.ts
@@ -1,14 +1,14 @@
 const ROUTES = {
-    HOME : "/",
-    SIGN_IN: "/sign-in",
-    SIGN_UP: "/sign-up",
-    TAG: (id: string) => `/tags/${id}`,
-    ASK_QUESTIONS: "/ask-question",
-    QUESTIONS: (id: string) => `/questions/${id}`,
-    PROFILE: (id: string) => `/profile/${id}`,
-    COLLECTION: "/collections",
-    COMMUNITY: "/community",
-    TAGS: "/tags",
-}
+  HOME: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+  TAG: (id: string) => `/tags/${id}`,
+  ASK_QUESTIONS: "/ask-question",
+  QUESTIONS: (id: string) => `/questions/${id}`,
+  PROFILE: (id: string) => `/profile/${id}`,
+  COLLECTION: "/collections",
+  COMMUNITY: "/community",
+  TAGS: "/tags",
+};
 
 export default ROUTES;
