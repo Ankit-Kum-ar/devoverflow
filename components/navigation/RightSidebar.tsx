2a66e5bd8d88482a491cca081c85bfd46cb7a0d4
Ankit-0009 <ankitcode2511@gmail.com>
2026-08-09 17:35:34 +0530

    Hot Question implementation in the right sidebar component using server action

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -1,50 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
 import TagCard from "../cards/TagCard";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import DataRenderer from "../DataRenderer";
+import ROUTES from "@/app/constants/route";
 
-const RightSidebar = () => {
-  const hotQuestions = [
-    { _id: "1", title: "How to center a div in CSS?" },
-    {
-      _id: "2",
-      title: "What is the difference between let and var in JavaScript?",
-    },
-    { _id: "3", title: "How to create a responsive layout with Flexbox?" },
-    { _id: "4", title: "What are the new features in ES6?" },
-    { _id: "5", title: "How to optimize React performance?" },
-  ];
-
-  const popularTags = [
-    { _id: "1", name: "JavaScript", questions: 1200 },
-    { _id: "2", name: "CSS3", questions: 900 },
-    { _id: "3", name: "React", questions: 1500 },
-    { _id: "4", name: "Node.js", questions: 800 },
-    { _id: "5", name: "Python", questions: 2000 },
-  ];
+const RightSidebar = async () => {
+  const { success, data: hotQuestions, error } = await getHotQuestions();
   return (
     <section className="pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="flex mt-7 w-full flex-col gap-[30px]">
-          {hotQuestions.map(({ _id, title }) => (
-            <Link
-              href={`/question/${_id}`}
-              key={_id}
-              className="flex cursor-pointer items-center justify-between gap-7"
-            >
-              <p className="body-medium text-dark500_light700">{title}</p>
-              <Image
-                src="/icons/chevron-right.svg"
-                alt="chevron-right"
-                width={20}
-                height={20}
-                className="invert-colors"
-              />
-            </Link>
-          ))}
+          <DataRenderer
+            data={hotQuestions}
+            empty={{
+              title: "No Question Found",
+              message: "No Question has been asked yet.",
+            }}
+            success={success}
+            error={error}
+            render={(hotQuestions) => (
+              <div className="mt-7 flex w-full flex-col gap-[30px]">
+                {hotQuestions.map(({ _id, title }) => (
+                  <Link
+                    href={ROUTES.QUESTIONS(_id)}
+                    key={_id}
+                    className="flex cursor-pointer items-center justify-between gap-7"
+                  >
+                    <p className="body-medium text-dark500_light700 line-clamp-2">
+                      {title}
+                    </p>
+                    <Image
+                      src="/icons/chevron-right.svg"
+                      alt="chevron-right"
+                      width={20}
+                      height={20}
+                      className="invert-colors"
+                    />
+                  </Link>
+                ))}
+              </div>
+            )}
+          />
         </div>
       </div>
-      <div className="mt-16 ">
+      {/* <div className="mt-16 ">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
           {popularTags.map(({ _id, name, questions }) => (
@@ -58,7 +59,7 @@ const RightSidebar = () => {
             />
           ))}
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -20,6 +20,7 @@ import type { Filter } from "mongodb";
 import Question, { IQuestionDoc } from "@/database/question.model";
 import Tag, { ITagDoc } from "@/database/tag.model";
 import TagQuestion from "@/database/tag-question.model";
+import dbConnect from "../mongoose";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -323,6 +324,24 @@ export async function incrementViews(
     return {
       success: true,
       data: { views: question.views },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getHotQuestions(): Promise<
+  ActionResponse<QuestionType[]>
+> {
+  try {
+    await dbConnect();
+    const questions = await Question.find()
+      .sort({ views: -1, upvotes: -1 })
+      .limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(questions)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
