48fdfd07ee4069f8d9266cef0308574294b17681
Ankit-0009 <ankitcode2511@gmail.com>
2026-08-15 22:48:19 +0530

    Interaction action implementation

--- database/interaction.model.ts
+++ database/interaction.model.ts
@@ -1,16 +1,28 @@
-import { model, models, Schema, Types } from "mongoose";
+import { model, models, Schema, Types, Document } from "mongoose";
 
-interface Iinteraction {
+interface IInteraction {
   user: Types.ObjectId;
   action: string;
   actionId: Types.ObjectId; // ID of the question or answer involved in the interaction
-  actionType: "question" | "answer"; // Type of the action (question or answer)
+  actionType: string; // Type of the action (question or answer)
 }
+
+export interface IInteractionDoc extends IInteraction, Document {}
+export const InteractionActionEnums = [
+  "view",
+  "upvote",
+  "downvote",
+  "bookmark",
+  "post",
+  "edit",
+  "delete",
+  "search",
+] as const;
 
 const interactionSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    action: { type: String, required: true },
+    action: { type: String, enum: InteractionActionEnums, required: true },
     actionId: { type: Schema.Types.ObjectId, required: true }, // ID of the question or answer involved in the interaction
     actionType: { type: String, enum: ["question", "answer"], required: true }, // Type of the action (question or answer)
   },
@@ -18,5 +30,5 @@ const interactionSchema = new Schema(
 );
 
 const Interaction =
-  models?.Interaction || model<Iinteraction>("Interaction", interactionSchema);
+  models?.Interaction || model<IInteraction>("Interaction", interactionSchema);
 export default Interaction;
